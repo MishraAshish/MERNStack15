4dc293fc3a3f2ade3fab38ee53df234c5014340f
Ashish <ash.ishu025@gmail.com>
2023-04-07 21:27:03 +0530

    js basics

---
+++ first.js
@@ -0,0 +1,33 @@
+//javascript is functional programming language - it behaviour oriented
+
+console.log(pi);
+console.log(Rectangle(5,6));
+
+function Rectangle(length, breadth) {
+    return length*breadth;
+}
+
+console.log(Rectangle(5,6));
+
+var pi = 3.14; //variable declaration
+
+function Circle(radius) {
+    return pi*radius*radius;
+}
+ 
+
+
+//create a circle function to give circle area
+
+
+//classes and methods - object oriented
+// class Area {
+//     int length, breadth, radius;
+
+//     Rectangle(l,b)
+//     Square(l)
+//     Circle(radius)
+// }
+
+// Area rectObj = new Area();
+// reactObj.Rectangle(5,6);
---
+++ jsBasics.js
@@ -0,0 +1,30 @@
+// compiled + interpreted language (hoisting)
+
+// variables declared usig var and any type of data can be assigned
+var test = 25;
+
+// auto casting or dynamic type casting
+var test = "Garrick";
+
+// variables identifier names are case sensitive
+var Test = "David";
+console.log(test)
+console.log(Test)
+
+// identifier names can be alphanumeric but should not start with a number
+var Test1 = "David";
+
+// $ and _ - special chars are allowed
+// console.log(_test)
+// console.log($Test)
+
+
+// - ctrl+/ (toogle to do comment of one line)
+
+/* --- code --- */ //for multiline comments
+
+/*
+var Test = "David";
+console.log(test)
+console.log(Test)
+*/
