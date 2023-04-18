5d8ca4357fd8fb6b24cb7338ee90cca1b75a1857
Ashish <ash.ishu025@gmail.com>
2023-04-18 21:18:31 +0530

    nested function

--- callBackandNestedFunctions.js
+++ callBackandNestedFunctions.js
@@ -27,3 +27,31 @@ setTimeout(function() {
 
 
 //write a callback function to print details of the user, calling function should be GetUserInfo
+
+
+// nested functions do hold the scope of outerfunctions variables or properties 
+//javascript currying
+var e = 15; //global scope 
+
+function sum1(a){
+
+  return function sum2(b){
+
+    return function sum3(c){
+      // outer functions scope
+      return function sum4(d){
+        // local scope
+        return a + b + c + d + e;
+      }
+    }
+  }
+}
+
+var outPut = sum1(5)(4)(3)(2)
+console.log(outPut)
+
+var sm2 = sum1(5)
+var sm3 = sm2(4)
+var sm4 = sm3(3)
+var smOutput = sm4(2)
+console.log(smOutput)
