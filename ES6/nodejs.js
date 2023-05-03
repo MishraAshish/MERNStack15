b42fff70cb66e9f12b6aa5ee021da6b6b2693ec7
Ashish <ash.ishu025@gmail.com>
2023-05-03 21:59:58 +0530

    node api

---
+++ ES6/nodejs.js
@@ -0,0 +1,30 @@
+//NODE - is a framework to run js on server, also is able to provide everything that 
+//we need to build an entertprse level application
+//I/O - operations
+//db connections
+//file manipulations
+//creating web servers
+//creating http modules
+//etc...
+
+
+//NodeJs - JRE -> whats the difference?
+
+//real time time systems - node js
+//100s of connections linkdin - can share information including text, images, videos, etc
+//node(live, light weight servers) base systems- points on varios servers to store and serve the information
+//majorly it uses flat file structure to save and fetch data
+//event loop based high performant single threaded framework
+
+//limitations -
+//all the task where calculation is involved
+
+//service oriented architecture
+//backend is termed as API or services and responds with data (json) over RESTFul/https channels
+//fronted is single page, contains logic to generate UI on the fly with the help of server data
+
+
+//executes the node module - Boubacar
+//Tool to run js - Alex
+//scripting language works for html - Tien
+//executing js code - Sohail
